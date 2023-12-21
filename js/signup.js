f7e0886b4f12560fcca2f784a608ad1b019f2356
Attah-Olottah Eneojo Hosea <attaholottaheneojo@gmail.com>
2023-12-21 15:20:15 +0000

    formatted the code structure

--- js/signup.js
+++ js/signup.js
@@ -1,15 +1,15 @@
-const togglePasswordButton = document.getElementById('togglePassword');
-const passwordInput = document.getElementById('password');
-const passwordToggleIcon = document.getElementById('passwordToggleIcon');
+const togglePasswordButton = document.getElementById("togglePassword");
+const passwordInput = document.getElementById("password");
+const passwordToggleIcon = document.getElementById("passwordToggleIcon");
 
-togglePasswordButton.addEventListener('click', function () {
-  if (passwordInput.type === 'password') {
-    passwordInput.type = 'text';
-    passwordToggleIcon.classList.remove('bi-eye-fill');
-    passwordToggleIcon.classList.add('bi-eye-slash-fill');
+togglePasswordButton.addEventListener("click", function () {
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    passwordToggleIcon.classList.remove("bi-eye-fill");
+    passwordToggleIcon.classList.add("bi-eye-slash-fill");
   } else {
-    passwordInput.type = 'password';
-    passwordToggleIcon.classList.remove('bi-eye-slash-fill');
-    passwordToggleIcon.classList.add('bi-eye-fill');
+    passwordInput.type = "password";
+    passwordToggleIcon.classList.remove("bi-eye-slash-fill");
+    passwordToggleIcon.classList.add("bi-eye-fill");
   }
 });
